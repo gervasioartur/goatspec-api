8c82eb4af373640358fac553f3d7df99b1c77942
dombo99 <202204259818@alunos.estacio.br>
2024-07-12 23:54:00 -0300

    refactor: improve code quality

--- src/test/java/com/goatspec/infrastructure/gateways/mappers/RoleEntityMapperTests.java
+++ src/test/java/com/goatspec/infrastructure/gateways/mappers/RoleEntityMapperTests.java
@@ -1,17 +1,11 @@
 package com.goatspec.infrastructure.gateways.mappers;
 
-import com.goatspec.domain.Enums.GenderEnum;
-import com.goatspec.domain.Enums.RoleEnum;
 import com.goatspec.domain.entities.Role;
-import com.goatspec.domain.entities.User;
 import com.goatspec.infrastructure.persisntence.entoties.RoleEntity;
-import com.goatspec.infrastructure.persisntence.entoties.UserEntity;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Date;
 
 @SpringBootTest
 public class RoleEntityMapperTests {
