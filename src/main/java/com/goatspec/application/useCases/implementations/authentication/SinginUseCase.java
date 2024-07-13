98439583e1dd56f76a8f2e4537daabecabb9c9ea
dombo99 <202204259818@alunos.estacio.br>
2024-07-13 08:59:55 -0300

    feat: ensure SinginUseCase throws BusinessException if the user does not exist

--- src/main/java/com/goatspec/application/useCases/implementations/authentication/SinginUseCase.java
+++ src/main/java/com/goatspec/application/useCases/implementations/authentication/SinginUseCase.java
@@ -1,4 +1,22 @@
 package com.goatspec.application.useCases.implementations.authentication;
 
-public class SinginUseCase {
+import com.goatspec.application.gateways.user.IUserGateway;
+import com.goatspec.application.useCases.contracts.authentication.ISinginUseCase;
+import com.goatspec.domain.entities.user.User;
+import com.goatspec.domain.entities.user.UserAccount;
+import com.goatspec.domain.exceptions.BusinessException;
+
+public class SinginUseCase implements ISinginUseCase {
+    private final IUserGateway userGateway;
+
+    public SinginUseCase(IUserGateway userGateway) {
+        this.userGateway = userGateway;
+    }
+
+    @Override
+    public UserAccount singin(String cpf, String password) {
+        User user = userGateway.findUserByCpf(cpf);
+        if (user == null) throw  new BusinessException("Bad credentials");
+        return null;
+    }
 }
---
+++ src/test/java/com/goatspec/application/useCases/implementations/authentication/SinginUseCaseTests.java
@@ -0,0 +1,39 @@
+package com.goatspec.application.useCases.implementations.authentication;
+
+import com.goatspec.application.gateways.user.IUserGateway;
+import com.goatspec.application.useCases.contracts.authentication.ISinginUseCase;
+import com.goatspec.domain.exceptions.BusinessException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+@SpringBootTest
+class SinginUseCaseTests {
+    private ISinginUseCase singinUseCase;
+    @MockBean
+    private IUserGateway userGateway;
+
+    @BeforeEach
+    void setUp() {
+        this.singinUseCase = new SinginUseCase(userGateway);
+    }
+
+    @Test
+    @DisplayName("Should throw business exception i the user does not exists")
+    void shouldThrowBusinessExceptionIfUserDoesNotExist() {
+        String cpf =  "any_cpf";
+        String password = "any_password";
+        Mockito.when(this.userGateway.findUserByCpf(cpf)).thenReturn(null);
+        Throwable exception = Assertions.catchThrowable(() ->  this.singinUseCase.singin(cpf,password));
+        Assertions.assertThat(exception).isInstanceOf(BusinessException.class);
+        Assertions.assertThat(exception.getMessage()).isEqualTo("Bad credentials");
+        Mockito.verify(this.userGateway, Mockito.times(1)).findUserByCpf(cpf);
+    }
+
+}
