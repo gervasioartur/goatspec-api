7afa3a0049c768059f58057395c960ba9f96d27e
dombo99 <202204259818@alunos.estacio.br>
2024-07-12 23:02:59 -0300

    feat: ensure UserEntityMapper return user domain object

--- src/test/java/com/goatspec/infrastructure/gateways/mappers/UserEntityMapperTests.java
+++ src/test/java/com/goatspec/infrastructure/gateways/mappers/UserEntityMapperTests.java
@@ -3,6 +3,7 @@ package com.goatspec.infrastructure.gateways.mappers;
 import com.goatspec.domain.Enums.GenderEnum;
 import com.goatspec.domain.Enums.RoleEnum;
 import com.goatspec.domain.entities.User;
+import com.goatspec.infrastructure.persisntence.entoties.RoleEntity;
 import com.goatspec.infrastructure.persisntence.entoties.UserEntity;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -26,6 +27,34 @@ public class UserEntityMapperTests {
         Assertions.assertThat(toCreateUserEntity.getRegistration()).isEqualTo(toCreateUserDomainObject.registration());
         Assertions.assertThat(toCreateUserEntity.getName()).isEqualTo(toCreateUserDomainObject.name());
         Assertions.assertThat(toCreateUserEntity.getDateOfBirth()).isEqualTo(toCreateUserDomainObject.dateOfBirth());
+        Assertions.assertThat(toCreateUserEntity.getPassword()).isEqualTo(toCreateUserDomainObject.password());
+    }
+
+    @Test
+    @DisplayName("Should return user domain object")
+    void shouldReturnUserDomainObject() {
+        User toCreateUserDomainObject = new User("any_cpf", "any_email", "any_create_registration", "any_name", new Date(), GenderEnum.MALE.getValue(), RoleEnum.TEACHER.getValue(), "any_password");
+
+        RoleEntity savedRoleEntity = RoleEntity.builder().name("any_name").active(true).build();
+        UserEntity toCreateUserEntity = UserEntity
+                .builder()
+                .cpf(toCreateUserDomainObject.cpf())
+                .email(toCreateUserDomainObject.email())
+                .registration(toCreateUserDomainObject.registration())
+                .name(toCreateUserDomainObject.name())
+                .dateOfBirth(toCreateUserDomainObject.dateOfBirth())
+                .gender(toCreateUserDomainObject.gender())
+                .password(toCreateUserDomainObject.password())
+                .build();
+
+        toCreateUserDomainObject = mapper.toUserDomainObject(toCreateUserEntity,savedRoleEntity);
+
+        Assertions.assertThat(toCreateUserEntity.getCpf()).isEqualTo(toCreateUserDomainObject.cpf());
+        Assertions.assertThat(toCreateUserEntity.getEmail()).isEqualTo(toCreateUserDomainObject.email());
+        Assertions.assertThat(toCreateUserEntity.getRegistration()).isEqualTo(toCreateUserDomainObject.registration());
+        Assertions.assertThat(toCreateUserEntity.getName()).isEqualTo(toCreateUserDomainObject.name());
+        Assertions.assertThat(toCreateUserEntity.getDateOfBirth()).isEqualTo(toCreateUserDomainObject.dateOfBirth());
+        Assertions.assertThat(savedRoleEntity.getName()).isEqualTo(toCreateUserDomainObject.role());
         Assertions.assertThat(toCreateUserEntity.getPassword()).isEqualTo(toCreateUserDomainObject.password());
     }
 }
