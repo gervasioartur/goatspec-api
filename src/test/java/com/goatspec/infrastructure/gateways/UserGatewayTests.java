5be121b2b58b07e266cb7cb297265c716aedec12
dombo99 <202204259818@alunos.estacio.br>
2024-07-12 23:22:18 -0300

    chore: add code coverage

--- src/test/java/com/goatspec/infrastructure/gateways/UserGatewayTests.java
+++ src/test/java/com/goatspec/infrastructure/gateways/UserGatewayTests.java
@@ -20,6 +20,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootTest
 class UserGatewayTests {
