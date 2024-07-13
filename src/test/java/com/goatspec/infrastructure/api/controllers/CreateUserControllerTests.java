70dadeb536f3a20bf8e2a268a786504a65d93d12
dombo99 <202204259818@alunos.estacio.br>
2024-07-13 04:00:06 -0300

    test: add gender field validations

--- src/test/java/com/goatspec/infrastructure/api/controllers/CreateUserControllerTests.java
+++ src/test/java/com/goatspec/infrastructure/api/controllers/CreateUserControllerTests.java
@@ -256,4 +256,44 @@ class CreateUserControllerTests {
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("body", Matchers.is("The field 'date of birth' is required!")));
     }
+
+    @Test
+    @DisplayName("Should return bad request if gender is empty")
+    void shouldReturnBadRequestIfGenderIsEmpty() throws Exception {
+        CreateUserRequest request = new CreateUserRequest("32635892024", "gervasio@gmail.com", "any_registration",
+                "any_name", new Date(), null, RoleEnum.TEACHER.getValue(), "any_password");
+
+        String json = new ObjectMapper().writeValueAsString(request);
+
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post(USER_API)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mvc
+                .perform(requestBuilder)
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("body", Matchers.is("The field 'gender' is required!")));
+    }
+
+    @Test
+    @DisplayName("Should return bad request if gender is null")
+    void shouldReturnBadRequestIfGenderIsNull() throws Exception {
+        CreateUserRequest request = new CreateUserRequest("32635892024", "gervasio@gmail.com", "any_registration",
+                "any_name", new Date(), null, RoleEnum.TEACHER.getValue(), "any_password");
+
+        String json = new ObjectMapper().writeValueAsString(request);
+
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post(USER_API)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mvc
+                .perform(requestBuilder)
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("body", Matchers.is("The field 'gender' is required!")));
+    }
 }
