e550a39cb04c5a08f88945dbd92f661c2051ed92
dombo99 <202204259818@alunos.estacio.br>
2024-07-13 00:43:57 -0300

    test: ensure AuthenticationGateway returns accessToken

---
+++ src/test/java/com/goatspec/infrastructure/gateways/AuthenticationGatewayTests.java
@@ -0,0 +1,89 @@
+package com.goatspec.infrastructure.gateways;
+
+import com.goatspec.domain.Enums.GenderEnum;
+import com.goatspec.domain.Enums.RoleEnum;
+import com.goatspec.domain.entities.User;
+import com.goatspec.infrastructure.gateways.helpers.token.GenerateToken;
+import com.goatspec.infrastructure.persisntence.entities.RoleEntity;
+import com.goatspec.infrastructure.persisntence.entities.UserEntity;
+import org.springframework.security.core.Authentication;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.*;
+
+@SpringBootTest
+class AuthenticationGatewayTests {
+    private AuthenticationGateway authenticationGateway;
+    @MockBean
+    private AuthenticationManager authenticationManager;
+    @MockBean
+    private GenerateToken generateToken;
+
+    @BeforeEach
+     void setUp() {
+        this.authenticationGateway = new AuthenticationGateway(authenticationManager, generateToken);
+    }
+
+    @Test
+    @DisplayName("Should return accestoken")
+    void shouldReturnAccessToken() {
+        String username = "any_username";
+        String password = "any_password";
+        String accessToken = UUID.randomUUID().toString();
+
+        Authentication authentication =  new Authentication() {
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                return List.of();
+            }
+
+            @Override
+            public Object getCredentials() {
+                return null;
+            }
+
+            @Override
+            public Object getDetails() {
+                return null;
+            }
+
+            @Override
+            public Object getPrincipal() {
+                return null;
+            }
+
+            @Override
+            public boolean isAuthenticated() {
+                return true;
+            }
+
+            @Override
+            public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+
+            }
+
+            @Override
+            public String getName() {
+                return "";
+            }
+        };
+
+        Mockito.when(this.authenticationManager.authenticate(Mockito.any(UsernamePasswordAuthenticationToken.class))).thenReturn(authentication);
+        Mockito.when(this.generateToken.generate(username)).thenReturn(accessToken);
+        
+        String accessTokenResult = this.authenticationGateway.authenticate(username,password);
+
+        Assertions.assertThat(accessTokenResult).isEqualTo(accessToken);
+        Mockito.verify(this.authenticationManager).authenticate(Mockito.any(UsernamePasswordAuthenticationToken.class));
+    }
+
+}
