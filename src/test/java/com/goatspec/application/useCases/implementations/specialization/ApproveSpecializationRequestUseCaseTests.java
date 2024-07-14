234b1bad7bf0046a808b8f739727f37648835610
dombo99 <202204259818@alunos.estacio.br>
2024-07-14 07:05:54 -0300

    test: ensure ApproveSpecializationRequestUseCase throws not found exception if the specialization is not present in the system

---
+++ src/test/java/com/goatspec/application/useCases/implementations/specialization/ApproveSpecializationRequestUseCaseTests.java
@@ -0,0 +1,41 @@
+package com.goatspec.application.useCases.implementations.specialization;
+
+import com.goatspec.application.gateways.specialization.ISpecializationGateway;
+import com.goatspec.application.useCases.contracts.specialization.IApproveSpecializationRequestUseCase;
+import com.goatspec.domain.exceptions.NotFoundException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.UUID;
+
+@SpringBootTest
+class ApproveSpecializationRequestUseCaseTests {
+    private IApproveSpecializationRequestUseCase approveSpecializationRequestUseCase;
+    @MockBean
+    private ISpecializationGateway specializationGateway;
+
+    @BeforeEach
+    void setUp() {
+        approveSpecializationRequestUseCase = new ApproveSpecializationRequestUseCase(specializationGateway);
+    }
+
+    @Test
+    @DisplayName("Should throw not found exception if the Specialization request does not exist")
+    void shouldThrowNotFoundExceptionIfTheSpecializationRequestDoesNotExist() {
+        UUID specializationRequestId = UUID.randomUUID();
+
+        Mockito.when(this.specializationGateway.findById(specializationRequestId)).thenReturn(null);
+
+        Throwable exception = Assertions.catchThrowable(()-> this.approveSpecializationRequestUseCase.approve(specializationRequestId));
+
+        Assertions.assertThat(exception).isInstanceOf(NotFoundException.class);
+        Assertions.assertThat(exception.getMessage()).contains("Couldn't find Specialization request.");
+        Mockito.verify(this.specializationGateway, Mockito.times(1)).findById(specializationRequestId);
+    }
+
+}
