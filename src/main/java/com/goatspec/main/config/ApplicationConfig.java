a7d23ca9dda47a4c6f2ca053db0752abb769ed14
dombo99 <202204259818@alunos.estacio.br>
2024-07-14 22:28:23 -0300

    feat: configuring email sender

--- src/main/java/com/goatspec/main/config/ApplicationConfig.java
+++ src/main/java/com/goatspec/main/config/ApplicationConfig.java
@@ -1,6 +1,7 @@
 package com.goatspec.main.config;
 
 import com.goatspec.application.gateways.authentication.IAuthenticationGateway;
+import com.goatspec.application.gateways.email.ISendEmailGateway;
 import com.goatspec.application.gateways.encrypt.IPasswordEncoderGateway;
 import com.goatspec.application.gateways.user.IUserGateway;
 import com.goatspec.application.useCases.contracts.authentication.IGetLoggedUserInfoUseCase;
@@ -8,6 +9,7 @@ import com.goatspec.application.useCases.contracts.authentication.ISinginUseCase
 import com.goatspec.application.useCases.implementations.authentication.GetLoggedUserInfoUseCase;
 import com.goatspec.application.useCases.implementations.authentication.SinginUseCase;
 import com.goatspec.infrastructure.gateways.authetication.AuthenticationGateway;
+import com.goatspec.infrastructure.gateways.email.SendEmailGateway;
 import com.goatspec.infrastructure.gateways.encrypt.PasswordEncoderGateway;
 import com.goatspec.infrastructure.gateways.helpers.security.SingKey;
 import com.goatspec.infrastructure.gateways.helpers.token.*;
@@ -15,8 +17,11 @@ import com.goatspec.infrastructure.gateways.mappers.UserEntityMapper;
 import com.goatspec.infrastructure.gateways.user.LoadUserByUsernameGateway;
 import com.goatspec.infrastructure.persisntence.repositories.IUserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -25,9 +30,17 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Properties;
+
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
+    @Value("${app.email.sender}")
+    private String appMailSender;
+
+    @Value("${app.email.password}")
+    private String password;
+
     @Bean
     public SingKey singKey() {
         return new SingKey();
@@ -120,4 +133,28 @@ public class ApplicationConfig {
     public IGetLoggedUserInfoUseCase getLoggedUserInfoUseCase(IAuthenticationGateway authenticationGateway) {
         return new GetLoggedUserInfoUseCase(authenticationGateway);
     }
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername(appMailSender);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+    @Bean
+    public ISendEmailGateway sendEmailGateway(JavaMailSender mailSender){
+        return new SendEmailGateway(mailSender);
+    }
+
 }
